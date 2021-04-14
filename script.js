38a9686d2937833f1236802095709602d6ec703f
Ratanak Uddam Chea <cheara01@ratanaks-air.lc.luther.edu>
2021-04-13 23:41:51 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+/* jshint esversion: 8 */
+/* jshint node: true */
+/* jshint browser: true */
+'use strict'
+
+const BASE_URL = "http://localhost:5000/api/v1/jokes"
+
+async function get_jokes() {
+    var category = document.getElementById("selCat").value;
+    var language = document.getElementById("selLang").value;
+    var number = document.getElementById("selNum").value;
+    var id = document.getElementById("jokeID").value;
+
+    if (id){
+        var jokes = await fetch(`${BASE_URL}/${category}/${language}/${number}/${id}`)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+        let row = document.querySelector("#jokes")
+        for (let index in jokes){
+        let td = document.createElement("p");
+        td.innerHTML = jokes[index];
+        row.appendChild(td);
+    }}
+    else{
+        var jokes = await fetch(`${BASE_URL}/${category}/${language}/${number}`)
+        .then(response => response.json())
+        .catch(error => console.log(error));
+        let row = document.querySelector("#jokes")
+        for (let index in jokes){
+        let td = document.createElement("p");
+        td.innerHTML = jokes[index];
+        row.appendChild(td);
+    }}
+}
